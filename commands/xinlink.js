2e18e155c5acd201f3abe2aa1a2c496ad9771a84
itsmehoaq <hoaqvvu06@gmail.com>
2024-05-16 00:56:22 +0700

    bb

--- commands/xinlink.js
+++ commands/xinlink.js
@@ -2,10 +2,12 @@ module.exports = {
     name: 'xinlink',
     description: 'xin link',
     async execute(message, args) {
+        // Check if the message is a reply
         if (message.reference) {
             try {
                 const originalMessage = await message.channel.messages.fetch(message.reference.messageId);
-                await message.channel.send(`${originalMessage.author} https://hoaq.s-ul.eu/LAM9H98F`);
+                await message.delete();
+                await originalMessage.reply(`https://hoaq.s-ul.eu/LAM9H98F`);
             } catch (error) {
                 console.error('Error fetching the original message:', error);
                 message.reply('đã có lỗi xảy ra :tf:');
