b2adcb4481523dfe55d6400ad8f2603f1de71613
itsmehoaq <hoaqvvu06@gmail.com>
2024-05-16 00:52:06 +0700

    b

--- commands/xinlink.js
+++ commands/xinlink.js
@@ -1,7 +1,17 @@
 module.exports = {
     name: 'xinlink',
     description: 'xin link',
-    execute(message, args) {
-            message.reply(`https://hoaq.s-ul.eu/LAM9H98F`);
+    async execute(message, args) {
+        if (message.reference) {
+            try {
+                const originalMessage = await message.channel.messages.fetch(message.reference.messageId);
+                await message.channel.send(`${originalMessage.author} https://hoaq.s-ul.eu/LAM9H98F`);
+            } catch (error) {
+                console.error('Error fetching the original message:', error);
+                message.reply('đã có lỗi xảy ra :tf:');
+            }
+        } else {
+            message.reply('đang xin ai link đấy?');
+        }
     },
 };
