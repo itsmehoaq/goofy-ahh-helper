4147e754b3f3065347bf679172d7778bf346aaf4
itsmehoaq <hoaqvvu06@gmail.com>
2024-05-16 04:25:31 +0700

    EoL update

--- commands/help.js
+++ commands/help.js
@@ -8,22 +8,22 @@ module.exports = {
             case 'tohex':
                 embed = new MessageEmbed()
                     .setColor('#e3e289')
-                    .setTitle('command info')
-                    .addFields({name: 'tohex', value: 'convert text to hex'}, {name: 'usage limit', value: 'everyone'})
+                    .setTitle('tohex')
+                    .addFields({name: 'wat do', value: 'convert text to hex'}, {name: 'usage limit', value: 'everyone'})
                     .setFooter({text: 'powered by HoaqGPT \u2122'});
                 break;
             case 'totext':
                 embed = new MessageEmbed()
                     .setColor('#e3e289')
-                    .setTitle('command info')
-                    .addFields({name: 'totext', value: 'convert hex to text'}, {name: 'usage limit', value: 'everyone'})
+                    .setTitle('totext')
+                    .addFields({name: 'wat do', value: 'convert hex to text'}, {name: 'usage limit', value: 'everyone'})
                     .setFooter({text: 'powered by HoaqGPT \u2122'});
                 break;
             case 'xinlink':
                 embed = new MessageEmbed()
                     .setColor('#e3e289')
-                    .setTitle('command info')
-                    .addFields({name: 'xinlink', value: 'xin sốt'}, {
+                    .setTitle('xinlink')
+                    .addFields({name: 'wat do', value: 'xin sốt'}, {
                         name: 'usage limit', value: 'everyone, chỉ hoạt động khi reply tin nhắn bằng lệnh này'
                     })
                     .setFooter({text: 'powered by HoaqGPT \u2122'});
@@ -31,8 +31,8 @@ module.exports = {
             case 'gasnotify':
                 embed = new MessageEmbed()
                     .setColor('#e3e289')
-                    .setTitle('command info')
-                    .addFields({name: 'gasnotify', value: 'thông báo giá xăng'}, {
+                    .setTitle('gasnotify')
+                    .addFields({name: 'wat do', value: 'thông báo giá xăng'}, {
                         name: 'usage limit', value: 'bot owner'
                     })
                     .setFooter({text: 'powered by HoaqGPT \u2122'});
@@ -40,16 +40,16 @@ module.exports = {
             case 'gasupdate':
                 embed = new MessageEmbed()
                     .setColor('#e3e289')
-                    .setTitle('command info')
+                    .setTitle('gasupdate')
                     .addFields({
-                        name: 'gasupdate', value: '**bot owner only**\ncập nhật file giá xăng'
+                        name: 'wat do', value: 'cập nhật file giá xăng'
                     }, {name: 'usage limit', value: 'bot owner'})
                     .setFooter({text: 'powered by HoaqGPT \u2122'});
                 break;
             default:
                 embed = new MessageEmbed()
                     .setColor('#e3e289')
-                    .setTitle('tao biết làm gì')
+                    .setTitle('wat the car doing')
                     .setDescription('dùng \`?help <command_name>\` để biết thêm thông tin về command')
                     .setFooter({text: 'powered by HoaqGPT \u2122'});
                 commands.forEach(command => {
